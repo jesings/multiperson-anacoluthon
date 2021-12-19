516f31201e05b9f565af2c94fbb5a7445fb1a888
jesings <34111484+jesings@users.noreply.github.com>
2021-12-19 15:24:18 -0500

    More networking stuff

--- src/net/servnet.rs
+++ src/net/servnet.rs
@@ -1,15 +1,25 @@
 use std::net::*;
+use std::time::*;
 use super::net_common::*;
+
+const ACCEPT_WAITTIME: u64 = 10;
 
 impl ServerNetstate {
     pub fn initialize_server(listen_ip_str: String) -> Self {
         let server_listener = TcpListener::bind(listen_ip_str).expect("Server was configured to bind to an invalid address!");
-        let streamvec = vec!();
+        let mut streamvec = vec!();
+
+        server_listener.set_nonblocking(true).expect("Could not set listening socket to be nonblocking!");
 
         //do some accepts to create the streams
+        let loopstarttime = Instant::now();
+        while Instant::now().duration_since(loopstarttime).as_secs() < ACCEPT_WAITTIME {
+            if let Ok(tcpl) = server_listener.accept() {
+                streamvec.push(tcpl);
+            }
+        }
 
         return Self {
-            listener: server_listener,
             streams: streamvec,
         };
     }
