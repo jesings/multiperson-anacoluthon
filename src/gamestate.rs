4c6031398d19733dc0cde82ddaf4a86eecbee020
jesings <34111484+jesings@users.noreply.github.com>
2021-12-19 15:46:48 -0500

    Added initial gamestate stuff

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -1,2 +1,12 @@
-struct GameState {
+use std::sync::*;
+use net::clinet::ClientNetstate;
+
+//GameState will always be wrapped in an arc, so its immutable members can be accessed without a lock or arc at all?
+//Other, mutable members can either be wrapped in a struct or not
+struct ClientGameState {
+    pub net: ClientNetstate,
+    //pub gamedata
+    //sdl_garbage
 }
+
+//gamedata struct, shared between client and server?
