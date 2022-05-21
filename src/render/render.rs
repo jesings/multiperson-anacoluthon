c23f326ebd516c87cc180b939903b20ab20aa9bb
anotherLostKitten <eggismagicchickensquared@gmail.com>
2022-05-21 10:40:41 -0400

    dynamic window resize

--- src/render/render.rs
+++ src/render/render.rs
@@ -1,8 +1,11 @@
 use crate::gamestate;
-use sdl2::rect::*;
 use sdl2::pixels::Color;
+use sdl2::rect::*;
 
-const TILEWIDTH: u32 = 40;
+const WINDOWWIDTH: u32 = 320;
+const WINDOWHEIGHT: u32 = 240;
+
+const TILEWIDTH: u32 = 16;
 const ITILEWIDTH: i32 = TILEWIDTH as i32;
 
 const PLAYERWIDTH: u32 = TILEWIDTH / 2;
@@ -17,33 +20,34 @@ impl gamestate::ClientGamestate {
         let pos = gd.players[pid].lock().expect("Could not lock player to get its position.").pos.clone();
 
         let mut canv = self.sdl.canv.lock().expect("Could not lock canvas for rendering!");
-        //canv.clear(); unneeded
+        canv.clear();
 
         //get dimensions of the canvas
         let canvsize: (u32, u32) = canv.output_size().expect("Could not get canvas size.");
-        let icanvsize = (canvsize.0 as i32, canvsize.1 as i32);
+
+        let upscale: u32 = std::cmp::min(canvsize.0 / WINDOWWIDTH, canvsize.1 / WINDOWHEIGHT);
+        let scaled_w: i32 = (upscale * WINDOWWIDTH) as i32;
+        let scaled_h: i32 = (upscale * WINDOWHEIGHT) as i32;
 
         //get corners of grid from that
-        let mut left_x = (icanvsize.0 - ITILEWIDTH)/2 % ITILEWIDTH - ITILEWIDTH; //initialize to corner position
-        let mut top_y = (icanvsize.1 - ITILEWIDTH)/2 % ITILEWIDTH - ITILEWIDTH; //initialize to corner position
-        if left_x == -ITILEWIDTH {left_x = 0;}
-        if top_y == -ITILEWIDTH {top_y = 0;}
-        let mut rendrect = Rect::new(left_x, top_y, TILEWIDTH, TILEWIDTH);
+        let left_x: i32 = (canvsize.0 as i32 - scaled_w) / 2; //initialize to corner position
+        let top_y: i32 = (canvsize.1 as i32 - scaled_h) / 2; //initialize to corner position
 
-        let player_tile_start_x = (icanvsize.0 / 2 - left_x - 1) / ITILEWIDTH * ITILEWIDTH + left_x; //rounded up
-        let player_tile_start_y = (icanvsize.1 / 2 - top_y - 1) / ITILEWIDTH * ITILEWIDTH + top_y; //rounded up
+        let mut rendrect = Rect::new(left_x, top_y, TILEWIDTH * upscale, TILEWIDTH * upscale);
+
+        let player_tile_start_x: i32 = left_x + WINDOWWIDTH as i32 / 2 / ITILEWIDTH * ITILEWIDTH * upscale as i32;
+        let player_tile_start_y: i32 = top_y + WINDOWHEIGHT as i32 / 2 / ITILEWIDTH * ITILEWIDTH * upscale as i32;
 
         //render tiles between corners
-        while rendrect.x() < icanvsize.0 {
+        while rendrect.x() < left_x + scaled_w {
             rendrect.set_y(top_y);
-            let col_index = (rendrect.x() - player_tile_start_x) / ITILEWIDTH + pos.0 as i32;
-            while rendrect.y() < icanvsize.1 {
+            let col_index = (rendrect.x() - player_tile_start_x) / (ITILEWIDTH * upscale as i32) + pos.0 as i32;
+            while rendrect.y() < top_y + scaled_h {
                 //eventually use copy or copy_ex for textures, get from the grid coord
-                
+
                 //this better be strength reduced by the compiler or rust is kinda lamer
-                let row_index = (rendrect.y() - player_tile_start_y) / ITILEWIDTH + pos.1 as i32;
+                let row_index = (rendrect.y() - player_tile_start_y) / (ITILEWIDTH * upscale as i32) + pos.1 as i32;
                 //now we have the position of the tile from its location relative to the player
-                
 
                 if col_index >= 0 && row_index >= 0 && col_index < gd.grid.cols as i32 && row_index < gd.grid.rows as i32 {
                     let grid_index = row_index as usize * gd.grid.cols + col_index as usize;
@@ -54,17 +58,18 @@ impl gamestate::ClientGamestate {
                 } else {
                     canv.set_draw_color(Color::RGB(0, 0, 0));
                 }
-                
+
                 if let Err(_) = canv.fill_rect(rendrect) {
                     eprintln!("Could not render tile at ({}, {}) from grid", col_index, row_index);
                 }
 
-                rendrect.offset(0, ITILEWIDTH);
+                rendrect.offset(0, ITILEWIDTH * upscale as i32);
             }
-            rendrect.offset(ITILEWIDTH, 0);
+            rendrect.offset(ITILEWIDTH * upscale as i32, 0);
         }
 
-        let curplayer_rect = Rect::new((icanvsize.0 - IPLAYERWIDTH) / 2, (icanvsize.1 - IPLAYERWIDTH) / 2, PLAYERWIDTH, PLAYERWIDTH);
+        let p_margin = IPLAYERWIDTH * upscale as i32 / 2;
+        let curplayer_rect = Rect::new(player_tile_start_x + p_margin, player_tile_start_y + p_margin, PLAYERWIDTH * upscale, PLAYERWIDTH * upscale);
 
         //now draw the players
         for wrappedplayer in &gd.players {
@@ -81,14 +86,19 @@ impl gamestate::ClientGamestate {
                 let otherpos = player.pos;
                 let xdelta = (pos.0 - otherpos.0) as i32;
                 let ydelta = (pos.1 - otherpos.1) as i32;
-                rightrect = Rect::new((icanvsize.0 - IPLAYERWIDTH) / 2 - xdelta * ITILEWIDTH, (icanvsize.1 - IPLAYERWIDTH) / 2 - ydelta * ITILEWIDTH, PLAYERWIDTH, PLAYERWIDTH);
+                rightrect = Rect::new(
+                    player_tile_start_x - xdelta * ITILEWIDTH * upscale as i32 + p_margin,
+                    player_tile_start_y - ydelta * ITILEWIDTH * upscale as i32 + p_margin,
+                    PLAYERWIDTH,
+                    PLAYERWIDTH,
+                );
             }
 
             if let Err(_) = canv.fill_rect(rightrect) {
                 eprintln!("Could not render player {}", player.pid);
             }
         }
-        
+
         canv.present();
     }
 }
